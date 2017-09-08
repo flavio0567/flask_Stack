f222dbe8ba286e7ef15c5a3c05f3bfeb23feef46
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-09-08 12:36:27 -0700

    Flask - Landing Page Assignment

---
+++ flask_fundamentals/landingpage/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_flask():
+  return render_template('index.html')
+
+@app.route('/ninjas')
+
+def ninjas():
+  return render_template('ninjas.html')
+
+@app.route('/dojos/new')
+
+def dojos_new():
+  return render_template('dojo.html')
+
+app.run(debug=True)
